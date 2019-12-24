82940f11a81e083ad3ef40e94a168818db8fd571
scotthoule <lehou0312@gmail.com>
2019-12-23 21:24:16 -0500

    Update get_patch_coordinates.sh

--- u24_lymphocyte/additional_code/get_ground_truth/get_patch_coordinates.sh
+++ u24_lymphocyte/additional_code/get_ground_truth/get_patch_coordinates.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-source ../conf/variables.sh
+source ../../conf/variables.sh
 
-SLIDES=../svs/
-IMG_FOLDER=./annotation_maps/
+SLIDES=${SVS_INPUT_PATH}
+IMG_FOLDER=${MODIFIED_HEATMAPS_PATH}
 
-PRED_FOLDER=../heatmaps_v2/gened/
-EMPTY_PRED_HEATMAP=../mix/empty_predictions/
+EMPTY_PRED_HEATMAP=${HEATMAP_TXT_OUTPUT_FOLDER}
 
 rm -rf patch_coordinates
 mkdir patch_coordinates
-for files in ${IMG_FOLDER}/*; do
-    SVS=`echo ${files} | awk -F'/' '{print $NF}' | awk -F'----' '{print $1}'`
-    USER=`echo ${files} | awk -F'/' '{print $NF}' | awk -F '----' '{print $2}'`
+for files in ${IMG_FOLDER}/*.png; do
+    if [ ! -f ${files} ]; then
+        continue;
+    fi
+    SVS=`echo ${files} | awk -F'/' '{print $NF}' | awk -F'.' '{print $1}'`
+    USER=`echo ${files} | awk -F'/' '{print $NF}' | awk -F '.' '{print $2}'`
     FULL_SVS_PATH=`ls -1 ${SLIDES}/${SVS}*.svs | head -n 1`
     if [ ! -f "${FULL_SVS_PATH}" ]; then
         FULL_SVS_PATH=`ls -1 ${SLIDES}/${SVS}*.tif | head -n 1`
@@ -23,21 +25,16 @@ for files in ${IMG_FOLDER}/*; do
         continue;
     fi
 
-    PREDICT=`ls -1 ${EMPTY_PRED_HEATMAP}/prediction-${SVS}* | head -n 1`
-    if [ ! -f "${PREDICT}" ]; then
-        echo Empty prediction ${PREDICT} does not exist
-        continue
-    fi
-
     WIDTH=` openslide-show-properties ${FULL_SVS_PATH} | grep "openslide.level\[0\].width"  | awk '{print substr($2,2,length($2)-2);}'`
     HEIGHT=`openslide-show-properties ${FULL_SVS_PATH} | grep "openslide.level\[0\].height" | awk '{print substr($2,2,length($2)-2);}'`
+    MPP=`openslide-show-properties ${FULL_SVS_PATH} | grep "aperio.MPP" | awk '{print substr($2,2,length($2)-2)}'`
     if [ "${WIDTH}" == "" ]; then
         echo Dimension of image ${FULL_SVS_PATH} is unknown
         continue
     fi
 
     matlab -nodisplay -singleCompThread -r \
-    "get_patch_coordinates('${SVS}', '${USER}', '${IMG}', '${PREDICT}', ${WIDTH}, ${HEIGHT}); exit;" \
+    "get_patch_coordinates('${files}', '${SVS}', '${USER}', '${IMG}', ${WIDTH}, ${HEIGHT}, ${MPP}); exit;" \
     </dev/null
 done
 
