72963a3695bb4d9e0fc7e71252445b0121c8fe33
scotthoule <lehou0312@gmail.com>
2019-12-23 21:26:17 -0500

    Update draw_patches.sh

--- u24_lymphocyte/additional_code/get_ground_truth/draw_patches.sh
+++ u24_lymphocyte/additional_code/get_ground_truth/draw_patches.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-SLIDES=../svs/
+source ../../conf/variables.sh
 
+SLIDES=../../data/svs/
+mkdir -p patches
+PAR=30
+
+N=0
 for files in ./patch_coordinates/*.txt; do
-    SVS=`echo ${files} | awk -F'/' '{print $NF}' | awk -F '----' '{print $1}'`
-    USER=`echo ${files} | awk -F'/' '{print $NF}' | awk -F '----' '{print $2}'`
+    SVS=`echo ${files} | awk -F'/' '{print $NF}' | awk -F '.' '{print $1}'`
     FULL_SVS_PATH=`ls -1 ${SLIDES}/${SVS}*.svs | head -n 1`
     if [ ! -f "${FULL_SVS_PATH}" ]; then
         FULL_SVS_PATH=`ls -1 ${SLIDES}/${SVS}*.tif | head -n 1`
@@ -12,10 +16,15 @@ for files in ./patch_coordinates/*.txt; do
     FULL_SVS_NAME=`echo ${FULL_SVS_PATH} | awk -F'/' '{print $NF}'`
     if [ ! -f "${FULL_SVS_PATH}" ]; then
         echo Image ${FULL_SVS_PATH} does not exist
-        continue;
+        continue
     fi
 
-    python draw_patches.py ${files} ${FULL_SVS_PATH}
+    nohup python draw_patches.py ${files} ${FULL_SVS_PATH} &
+    N=$((N+1))
+    if [ $N -ge $PAR ]; then
+        wait
+        N=0
+    fi
 done
 
 exit 0
