5a3d69acfc3aabbe9eb92627e5cced98761d88d1
scotthoule <lehou0312@gmail.com>
2019-12-24 11:23:11 -0500

    Update start.sh

--- u24_lymphocyte/additional_code/super_patch_extraction/start.sh
+++ u24_lymphocyte/additional_code/super_patch_extraction/start.sh
@@ -2,4 +2,7 @@
 
 source ../../conf/variables.sh
 
+matlab -nodisplay -singleCompThread -r "distribution_8x8_sample; exit;"
+matlab -nodisplay -singleCompThread -r "extract_super_patches; exit;"
+
 exit 0
